f5814921626fdb42c249a7daf237227c6ace920a
Milanie Bano <mlbano26@gmail.com>
2024-03-21 19:30:43 -0400

    Initial Commit

---
+++ iTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTask
+//
+//  Created by Milanie Bano on 2024-03-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
